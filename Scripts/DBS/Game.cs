5e3c17a5ba21ec4bcbc536972ef50594d643fbb5
JoshuaJDevine <jd8386@gmail.com>
2023-03-19 15:45:30 -0700

    Add Notes and Game file

---
+++ Scripts/DBS/Game.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Notes:
+ * Upgrade Tree:
+ * --- Skill tree idea: Mana Control
+ * --- Initially you can swap mana and they move back if no match
+ * --- Swap a mana up to two positions
+ * --- Swap a mana up to three positions
+ * --- Place your own trio of mana (random)
+ * --- Place your own duo of mana (random)
+ * --- Place your own mana (random)
+ * --- Place your own mana (choose color)
+ * --- Move a mana anywhere you want
+ *
+ * --- Matches = units
+ * --- Matching units = upgrade
+ *
+ * --- Skill tree idea: Unit controll
+ * --- Choose the upgrade path instead of it being randomgit 
+ */
+
+namespace DBS
+{
+    public class Game
+    {
+        
+    }
+}
